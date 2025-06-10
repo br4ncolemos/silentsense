a36fc21ddcc7815292ad52d3a5903271bec03283
br4ncolemos <juanbrancolemosss@gmail.com>
2025-06-10 10:11:22 -0300

    Update server.js

--- public/server/server.js
+++ public/server/server.js
@@ -11,7 +11,7 @@ const fs = require('fs'); // Filesystem para ler/escrever arquivos
 const multer = require('multer'); // Middleware para upload de arquivos
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(morgan('dev'));
